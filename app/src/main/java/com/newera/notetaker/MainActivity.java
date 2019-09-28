bb75e76f9554340a84fb16d0ab41ec642d31bd75
LeeBoonKong <lee.boon.kong@hotmail.com>
2019-09-28 11:50:24 +0800

    First Commit

---
+++ app/src/main/java/com/newera/notetaker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.newera.notetaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
